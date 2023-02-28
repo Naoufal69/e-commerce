0f61915a9227f52eb0797c86340502efc2a38af4
Naoufal69 <94354202+Naoufal69@users.noreply.github.com>
2023-02-28 16:27:20 +0100

    Récuperation des données en base

--- e-cross/src/components/index.tsx
+++ e-cross/src/components/index.tsx
@@ -1,7 +1,35 @@
+import { collection, query, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase.config";
 function Index() {
+  const [motocross, setMoto] = useState([] as MotoCross[]);
+  type MotoCross = {
+    date: number;
+    modele: string;
+    prix: number;
+    marque: string;
+    image: string;
+  };
+  const loadMotocross = async () => {
+    const q = query(collection(db, "motocross"));
+    const querySnapshot = await getDocs(q);
+    setMoto([])
+    querySnapshot.forEach((doc) => {
+      setMoto((motocross) => [...motocross, doc.data() as MotoCross]);
+    });
+  };
+  useEffect(() => {
+    loadMotocross();
+  }, []);
   return (
     <div>
-      <h1>Index</h1>
+      {motocross.map((motocross,index)=><div className="mb-5" key={index}>
+        <img src={motocross.image}/>
+        <div>{motocross.modele}</div>
+        <div>{new Intl.NumberFormat().format(motocross.prix)} €</div>
+        <div>{motocross.date}</div>
+        <div>{motocross.marque}</div>
+        </div>)}
     </div>
   );
 }
