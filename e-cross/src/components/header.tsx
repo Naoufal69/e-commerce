031ec351b87a6728a59a390cb778cd9a9a81c4ad
Naoufal69 <94354202+Naoufal69@users.noreply.github.com>
2023-02-28 14:23:06 +0100

    Remplacement de la balise div par header

--- e-cross/src/components/header.tsx
+++ e-cross/src/components/header.tsx
@@ -3,13 +3,13 @@ import account from '../assets/account.png'
 import caddi from '../assets/panier.png'
 function Header() {
   return (
-    <div className="h-20 bg-black flex justify-between">
+    <header className="h-20 bg-black flex justify-between">
       <img src={logo} alt="Image gauche" className="w-1/4 object-contain justify-start"/>
       <div className='flex w-3/4 justify-end'>
         <img src={caddi} alt="Image droite"/>
         <img src={account} alt="Image droite"/>
       </div>
-    </div>
+    </header>
   );
 }
 export default Header;
