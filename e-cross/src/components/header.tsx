3b22b0ac4b94a1997408f37672048454ea0eef51
Naoufal69 <94354202+Naoufal69@users.noreply.github.com>
2023-02-28 14:22:38 +0100

    Création d'un header

--- e-cross/src/components/header.tsx
+++ e-cross/src/components/header.tsx
@@ -1,4 +1,15 @@
+import logo from '../assets/logo.png'
+import account from '../assets/account.png'
+import caddi from '../assets/panier.png'
 function Header() {
-  return <div className="h-20 bg-black"></div>;
+  return (
+    <div className="h-20 bg-black flex justify-between">
+      <img src={logo} alt="Image gauche" className="w-1/4 object-contain justify-start"/>
+      <div className='flex w-3/4 justify-end'>
+        <img src={caddi} alt="Image droite"/>
+        <img src={account} alt="Image droite"/>
+      </div>
+    </div>
+  );
 }
 export default Header;
