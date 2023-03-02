c31811a20cd6b02da9159c8194c2afd664dd3433
Luka Gulmez <you@example.com>
2023-03-02 12:23:45 +0100

    Fin du header + responsivité de la page index/front

--- e-cross/src/components/header.tsx
+++ e-cross/src/components/header.tsx
@@ -4,13 +4,16 @@ import caddi from "../assets/panier.png";
 import { Link, NavLink } from "react-router-dom";
 function Header() {
   return (
-    <header className="h-20 w-full min-w-full bg-black flex justify-between">
+    <header className="h-20 w-full min-w-full bg-gradient-to-r from-zinc-700 via-black to-black flex justify-between">
       <div className="h-20">
         <Link to="/">
           <img src={logo} alt="Image gauche" className="h-20" />
         </Link>
       </div>
-      <div className="flex w-3/4 justify-end">
+      <div className="text-white m-auto font-mono text-5xl font-bold " >
+        E-Cross
+      </div>
+      <div className="flex justify-end">
         <img src={caddi} alt="Image droite" />
         <Link to="/Profile">
           <img src={account} alt="Image droite" className="h-14 mt-2" />
